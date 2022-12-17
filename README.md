0cbcb48059f1a0d59bc94a46f0d66fed9cc83cd0
benno <BennoCrafterDev@googlemail.com>
2022-12-17 22:14:16 +0100

    added some new asking features and impoved the find_closest_element by using a better grammer and the posibillity to change to my own find_closest_element method or the difflib method

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # CodeWizard
-Ask about a Problem or an ask and get an answer
+### What is CodeWizard?
+CodWizard is a simple language-model to help a developer by programmming problems.
+Until now its not very well, because it haven't much data. But i work hardly on it, to improve it.
+
+### What are the Possibilitys?
+
+- create for loop
+- create function
+- create variable
+- create list
+- print an element of a list
+- create function
+- save every single line of a file into a list
--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import find_closest_element
+import difflib
 
-data = open('example.txt').read().split("\n")
+own_method = True
+data = open('data.txt').read().split("\n")
 questions = []
 answers = []
 for element in data:
-    question, answer = element.split('|')
+    question, answer = element.split(';')
     questions.append(question)
     answers.append(answer)
 
 input = input("Input:")
-question = find_closest_element.find_closest_element(input, questions)
+# us difflib
+if not own_method:
+    question = difflib.get_close_matches(input, questions, n=5, cutoff=0.7)
+    question = question[0]
+# use own variant
+if own_method:
+    question = find_closest_element.find_closest_element(input, questions)
+
 answer = answers[(questions.index(question))]
 answer = answer.replace("$n", "\n").replace("$t", "\t")
 print(answer)
